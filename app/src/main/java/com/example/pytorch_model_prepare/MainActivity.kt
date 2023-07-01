5f5a851c83b25f3384838faed7b5aabe5a126128
Nafisur <nafisur1990@gmail.com>
2023-07-01 21:36:20 +0200

    working model and camera but predictions are wrong

--- app/src/main/java/com/example/pytorch_model_prepare/MainActivity.kt
+++ app/src/main/java/com/example/pytorch_model_prepare/MainActivity.kt
@@ -5,6 +5,9 @@ import android.content.pm.PackageManager
 import android.content.res.AssetManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Rect
+import android.media.Image
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -13,10 +16,8 @@ import android.util.Size
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.ImageCapture
-import androidx.camera.core.Preview
-import androidx.camera.core.VideoCapture
+import androidx.annotation.RequiresApi
+import androidx.camera.core.*
 import com.chaquo.python.PyObject
 import com.chaquo.python.Python
 import com.chaquo.python.android.AndroidPlatform
@@ -32,6 +33,8 @@ import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.pytorch_model_prepare.databinding.ActivityMainBinding
+import java.nio.ByteBuffer
+import java.util.concurrent.Executors
 
 
 class MainActivity : AppCompatActivity() {
@@ -40,24 +43,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
-//        val activityResultLauncher =
-//            registerForActivityResult(
-//                ActivityResultContracts.RequestMultiplePermissions())
-//            { permissions ->
-//                // Handle Permission granted/rejected
-//                var permissionGranted = true
-//                permissions.entries.forEach {
-//                    if (it.key in REQUIRED_PERMISSIONS && it.value == false)
-//                        permissionGranted = false
-//                }
-//                if (!permissionGranted) {
-//                    Toast.makeText(baseContext,
-//                        "Permission request denied",
-//                        Toast.LENGTH_SHORT).show()
-//                } else {
-//                    startCamera()
-//                }
-//            }
+
         if (allPermissionsGranted()) {
             Log.d("output", "all permissions granted")
             startCamera()
@@ -70,56 +56,61 @@ class MainActivity : AppCompatActivity() {
 //            activityResultLauncher.launch(REQUIRED_PERMISSIONS)
 //        }
 //        this is the python part of the code
+
+        // working code from here
+
         if (! Python.isStarted()) {
             Python.start(AndroidPlatform(this))
         }
 
-        var img: Bitmap = BitmapFactory.decodeStream(assets.open("image.jpg"))
-        val resizedBitmap = Bitmap.createScaledBitmap(img, 256, 256, true)
-        val normalizedBitmap = resizedBitmap.copy(Bitmap.Config.ARGB_8888, true)
-//        convert the image to a float array
-        var imgData: FloatArray = convertBitmapToFloatArray(normalizedBitmap)
-//        val imageTensor = preprocessImage(resizedBitmap)
-//        val inputTensor = Tensor.fromBlob(resizedBitmap, longArrayOf(1, resizedBitmap))
-        Log.d("output", "image loaded")
-//        load the pytorch model
-            var module = LiteModuleLoader.load(assetFilePath("model.ptl"))
-        Log.d("output", "model loaded")
+//        var img: Bitmap = BitmapFactory.decodeStream(assets.open("image.jpg"))
+//        val resizedBitmap = Bitmap.createScaledBitmap(img, 256, 256, true)
+//        val normalizedBitmap = resizedBitmap.copy(Bitmap.Config.ARGB_8888, true)
+////        convert the image to a float array
+//        var imgData: FloatArray = convertBitmapToFloatArray(normalizedBitmap)
+////        val imageTensor = preprocessImage(resizedBitmap)
+////        val inputTensor = Tensor.fromBlob(resizedBitmap, longArrayOf(1, resizedBitmap))
+//        Log.d("output", "image loaded")
+////        load the pytorch model
+//            var module = LiteModuleLoader.load(assetFilePath("model.ptl"))
+//        Log.d("output", "model loaded")
+//
+////        convert the image to tensor of shape [1, 3, 256, 256]
+//
+//        var inputTensor = Tensor.fromBlob(imgData, longArrayOf(1, 3, 256, 256))
+//        Log.d("output", "tensor loaded")
+//        Log.d("output", inputTensor.dtype().toString())
+//
+//        val outputTensor = module.forward(IValue.from(inputTensor)).toTuple()
+//        Log.d("output", "output loaded")
+////        val outputTuple = arrayOf(outputTensor)
+//        val out_3d = outputTensor[0].toTensor()
+//        val out_2d = outputTensor[1].toTensor()
+//        Log.d("output", out_3d.toString())
+////        convert the tensor to array
+//        val out_3d_array = out_3d.dataAsFloatArray
+////        Log.d("output", out_3d_array.toString())
+////        val out_2d_array = out_2d.getDataAsFloatArray()
+////        Log.d("output", out_3d_array.size.toString())
+////        Log.d("output", out_2d_array.toString())
+//        Log.d("output", "array loaded")
+//
+//// add chacopy and python script to the project for the next step
+//        val py = Python.getInstance()
+//        val obj: PyObject = py.getModule("plot")
+////        convert out_3d tensor to numpy array keeping the shape
+//
+//        val obj1: PyObject = obj.callAttr("main", out_3d_array, out_2d)
+//        Log.d("output", "python script loaded")
+//        val str = obj1.toString()
+////        Log.d("output", str)
+//        val data: ByteArray = Base64.getDecoder().decode(str)
+//        val bmp = BitmapFactory.decodeByteArray(data, 0, data.size)
+//        val image = findViewById<ImageView>(R.id.imageView)
+//        image.setImageBitmap(bmp)
 
-//        convert the image to tensor of shape [1, 3, 256, 256]
 
-        var inputTensor = Tensor.fromBlob(imgData, longArrayOf(1, 3, 256, 256))
-        Log.d("output", "tensor loaded")
-        Log.d("output", inputTensor.dtype().toString())
-
-        val outputTensor = module.forward(IValue.from(inputTensor)).toTuple()
-        Log.d("output", "output loaded")
-//        val outputTuple = arrayOf(outputTensor)
-        val out_3d = outputTensor[0].toTensor()
-        val out_2d = outputTensor[1].toTensor()
-        Log.d("output", out_3d.toString())
-//        convert the tensor to array
-        val out_3d_array = out_3d.dataAsFloatArray
-//        Log.d("output", out_3d_array.toString())
-//        val out_2d_array = out_2d.getDataAsFloatArray()
-//        Log.d("output", out_3d_array.size.toString())
-//        Log.d("output", out_2d_array.toString())
-        Log.d("output", "array loaded")
-
-// add chacopy and python script to the project for the next step
-        val py = Python.getInstance()
-        val obj: PyObject = py.getModule("plot")
-//        convert out_3d tensor to numpy array keeping the shape
-
-        val obj1: PyObject = obj.callAttr("main", out_3d_array, out_2d)
-        Log.d("output", "python script loaded")
-        val str = obj1.toString()
-//        Log.d("output", str)
-        val data: ByteArray = Base64.getDecoder().decode(str)
-        val bmp = BitmapFactory.decodeByteArray(data, 0, data.size)
-        val image = findViewById<ImageView>(R.id.imageView)
-        image.setImageBitmap(bmp)
-
+        // ???????????????? above works
 //        val obj1: PyObject = obj.callAttr("main", out_3d, out_2d)
 //        Log.d("output", "python script loaded")
 //        val str = obj1.toString()
@@ -178,6 +169,7 @@ class MainActivity : AppCompatActivity() {
         }
         return null
     }
+    @RequiresApi(Build.VERSION_CODES.O)
     private fun startCamera() {
         Log.d("output", "camera started")
         try {
@@ -199,6 +191,172 @@ class MainActivity : AppCompatActivity() {
                     it.setSurfaceProvider(viewBinding.viewFinder.surfaceProvider)
                 }
 
+            // image analysis
+
+            val  imageAnalyzer= ImageAnalysis.Builder()
+                 .setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888)
+                .setTargetResolution(Size(256, 256))
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build()
+
+            val analyzer = ImageAnalysis.Analyzer { image_input ->
+
+                // load the model
+                var module = LiteModuleLoader.load(assetFilePath("model.ptl"))
+                Log.d("output", "model loaded")
+//                 convert image to bitmap
+//                try {
+////                    print the size of the image
+//                    Log.d("output", image_input.width.toString())
+//                    Log.d("output", image_input.height.toString())
+//                    val planes = image_input.planes
+//                    Log.d("output", planes.size.toString())
+////                    double the capacity of the buffer
+//                    val buffer = planes[0].buffer
+//
+////                    log the size of the buffer
+//                    Log.d("output", buffer.capacity().toString())
+////                    make the buffer large enough for the input image
+//
+//                    val pixelStride = planes[0].pixelStride
+//                    val rowStride = planes[0].rowStride
+//                    val width = image_input.width
+//                    val height = image_input.height
+//
+//                    val bitmap = Bitmap.createBitmap(
+//                        width + (pixelStride - 1) * (width / pixelStride),
+//                        height,
+//                        Bitmap.Config.ALPHA_8
+//                    )
+//                    buffer.rewind()
+//                    bitmap.copyPixelsFromBuffer(buffer)
+//                    //center crop the image to 400x400
+//                    val croppedBitmap = Bitmap.createBitmap(400, 400, Bitmap.Config.ARGB_8888)
+//
+//                    val xOffset = (bitmap.width - 400) / 2
+//                    val yOffset = (bitmap.height - 400) / 2
+//
+//                    val canvas = Canvas(croppedBitmap)
+//                    canvas.drawBitmap(bitmap, Rect(xOffset, yOffset, xOffset + 400, yOffset + 400), Rect(0, 0, 400, 400), null)
+////                 resize the image to 256x256
+//                    val resizedBitmap = Bitmap.createScaledBitmap(croppedBitmap, 256, 256, true)
+//                    val normalizedBitmap = resizedBitmap.copy(Bitmap.Config.ARGB_8888, true)
+//                }
+//                catch (e: Exception){
+//                    Log.d("output", e.toString())
+//                }
+                val planes = image_input.planes
+                val buffer = planes[0].buffer
+                val bytes = ByteArray(buffer.capacity())
+
+                buffer.get(bytes)
+                val pixelStride = planes[0].pixelStride
+                val rowStride = planes[0].rowStride
+                val width = image_input.width
+                val height = image_input.height
+
+                val bitmap = Bitmap.createBitmap(
+                    width ,
+                    height,
+                    Bitmap.Config.ARGB_8888
+//                            Bitmap.Config.ALPHA_8
+
+                )
+//                get bitmap from preview view
+
+//                var bitmap2 = previewView.getbitmap()
+
+                buffer.rewind()
+//                bitmap.copyPixelsFromBuffer(buffer)
+                bitmap.copyPixelsFromBuffer(ByteBuffer.wrap(bytes))
+                image_input.close()
+
+                //center crop the image to 400x400
+                val croppedBitmap = Bitmap.createBitmap(400, 400, Bitmap.Config.ARGB_8888)
+
+                val xOffset = (bitmap.width - 400) / 2
+                val yOffset = (bitmap.height - 400) / 2
+
+                val canvas = Canvas(croppedBitmap)
+                canvas.drawBitmap(bitmap, Rect(xOffset, yOffset, xOffset + 400, yOffset + 400), Rect(0, 0, 400, 400), null)
+//                 resize the image to 256x256
+//                log the  size of the bitmap
+                Log.d("output", croppedBitmap.width.toString())
+                Log.d("output", croppedBitmap.height.toString())
+                val resizedBitmap = Bitmap.createScaledBitmap(croppedBitmap, 256, 256, true)
+                val normalizedBitmap = resizedBitmap.copy(Bitmap.Config.ARGB_8888, true)
+//        convert the image to a float array
+                var imgData: FloatArray = convertBitmapToFloatArray(normalizedBitmap)
+
+// testing som ecode here #########################################################################
+//                var img: Bitmap = BitmapFactory.decodeStream(assets.open("image.jpg"))
+//                val resizedBitmap = Bitmap.createScaledBitmap(img, 256, 256, true)
+//                val normalizedBitmap = resizedBitmap.copy(Bitmap.Config.ARGB_8888, true)
+////        convert the image to a float array
+//                var imgData: FloatArray = convertBitmapToFloatArray(normalizedBitmap)
+        Log.d("output", "image loaded")
+//        convert the image to tensor of shape [1, 3, 256, 256]
+
+                var inputTensor = Tensor.fromBlob(imgData, longArrayOf(1, 3, 256, 256))
+
+                // run the model
+//                var outputTensor = module.forward(IValue.from(inputTensor)).toTensor()
+                val outputTensor = module.forward(IValue.from(inputTensor)).toTuple()
+
+//                val outputTuple = arrayOf(outputTensor)
+//                try {
+//                    val out_3d = outputTensor[0].toTensor()
+//                    val out_2d = outputTensor[1].toTensor()
+//                    Log.d("output", "works")
+//                }catch (e: Exception){
+//                    Log.d("output", e.toString())
+//                }
+                val out_3d = outputTensor[0].toTensor()
+                val out_2d = outputTensor[1].toTensor()
+                Log.d("output", out_3d.toString())
+//        convert the tensor to array
+                val out_3d_array = out_3d.dataAsFloatArray
+
+
+
+
+                val py = Python.getInstance()
+                val obj: PyObject = py.getModule("plot")
+//        convert out_3d tensor to numpy array keeping the shape
+
+                val obj1: PyObject = obj.callAttr("main", out_3d_array, out_2d)
+                Log.d("output", "python script loaded")
+                val str = obj1.toString()
+//        Log.d("output", str)
+                try {
+                    val data: ByteArray = Base64.getDecoder().decode(str)
+                    val bmp = BitmapFactory.decodeByteArray(data, 0, data.size)
+                    val image_to_view = findViewById<ImageView>(R.id.imageView)
+                    image_to_view.setImageBitmap(bmp)
+
+                }
+                catch (e: Exception){
+                    Log.d("output", e.toString())
+                }
+                val data: ByteArray = Base64.getDecoder().decode(str)
+                val bmp = BitmapFactory.decodeByteArray(data, 0, data.size)
+                val image_to_view = findViewById<ImageView>(R.id.imageView)
+
+//                try {
+//                    image_to_view.setImageBitmap(bmp)
+//                    image_input.close()
+//                    Log.d("output", "no problem here")
+//                }
+//                catch (e: Exception){
+//                    Log.d("output", e.toString())
+//                }
+                image_to_view.setImageBitmap(bmp)
+//                image_input.close()
+            }
+            imageAnalyzer.setAnalyzer(Executors.newSingleThreadExecutor(), analyzer)
+
+
+
             // Select back camera as a default
             val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
 
@@ -208,8 +366,8 @@ class MainActivity : AppCompatActivity() {
 
                 // Bind use cases to camera
                 cameraProvider.bindToLifecycle(
-                    this, cameraSelector, preview)
-                        Log.d("output", "camera started")
+                    this, cameraSelector, preview, imageAnalyzer)
+                        Log.d("output", "life cycle bound")
 
 
             } catch(exc: Exception) {
